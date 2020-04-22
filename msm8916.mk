bf8f52d5294978939ed1ef0b996225a311baf851
althafvly <althafvly@gmail.com>
2020-04-22 23:18:57 +0530

    msm8916-common: Clean Up
    remove duplicate

--- msm8916.mk
+++ msm8916.mk
@@ -141,10 +141,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
-# DRM
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
-
 # Ebtables
 PRODUCT_PACKAGES += \
     ebtables \
